58063b90601ec84b858cf0e8b6bd32e3b399a72a
Valerio Andrea Chiodi <valerioandrea.chiodi@gmail.com>
2025-04-26 22:37:29 +0200

    aggiunto pc.css e relativo js

--- js/change.js
+++ js/change.js
@@ -1,19 +1,20 @@
-    function loadCSS() {
-        let userAgent = navigator.userAgent || navigator.vendor || window.opera;
-        let cssFile = "default.css"; // Default CSS
+function loadCSS() {
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    let cssFile = "css/default.css"; // Default CSS
 
-        if (/android/i.test(userAgent)) {
-            cssFile = "css/android.css"; // Android
-        } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            cssFile = "css/ios.css"; // iOS
-        }
-
-        let link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.type = "text/css";
-        link.href = cssFile;
-        document.head.appendChild(link);
+    if (/android/i.test(userAgent)) {
+        cssFile = "css/android.css"; // Android
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        cssFile = "css/ios.css"; // iOS
+    } else {
+        cssFile = "css/pc.css"; // PC (Windows, Mac, Linux)
     }
 
-    loadCSS();
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = cssFile;
+    document.head.appendChild(link);
+}
 
+loadCSS();
